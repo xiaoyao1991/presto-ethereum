db4da5d2cc1a54a9a18962cc752f2730228e51a4
Xiaoyao Qian <xqian@twitter.com>
2017-08-01 14:55:01 -0700

    index by block hash

--- src/main/java/im/xiaoyao/presto/ethereum/EthereumBlockRange.java
+++ src/main/java/im/xiaoyao/presto/ethereum/EthereumBlockRange.java
@@ -3,32 +3,30 @@ package im.xiaoyao.presto.ethereum;
 import com.facebook.presto.spi.predicate.Marker;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import sun.jvm.hotspot.oops.Mark;
 
-import java.util.Iterator;
-
-public class EthereumBlockRange implements Iterator<Long> {
+public class EthereumBlockRange {
     private final long startBlock;
     private final long endBlock;
-    private long cursor;
 
-    public EthereumBlockRange(Marker low, Marker high) {
+    public static EthereumBlockRange fromMarkers(Marker low, Marker high) {
+        long startBlock;
+        long endBlock;
         if (low.isLowerUnbounded()) {
-            this.startBlock = 1L;
+            startBlock = 1L;
         } else if (low.getBound() == Marker.Bound.EXACTLY) {
-            this.startBlock = (long) low.getValue();
+            startBlock = (long) low.getValue();
         } else if (low.getBound() == Marker.Bound.ABOVE) {
-            this.startBlock = (long) low.getValue() + 1L;
+            startBlock = (long) low.getValue() + 1L;
         } else {
             throw new IllegalArgumentException("Low bound cannot be BELOW");
         }
 
         if (high.isUpperUnbounded()) {
-            this.endBlock = -1L;
+            endBlock = -1L;
         } else if (high.getBound() == Marker.Bound.EXACTLY) {
-            this.endBlock = (long) high.getValue();
+            endBlock = (long) high.getValue();
         } else if (high.getBound() == Marker.Bound.BELOW) {
-            this.endBlock = (long) high.getValue() - 1L;
+            endBlock = (long) high.getValue() - 1L;
         } else {
             throw new IllegalArgumentException("High bound cannot be ABOVE");
         }
@@ -37,30 +35,25 @@ public class EthereumBlockRange implements Iterator<Long> {
             throw new IllegalArgumentException("Low bound is greater than high bound");
         }
 
-        this.cursor = this.startBlock;
+        return new EthereumBlockRange(startBlock, endBlock);
     }
 
-    public EthereumBlockRange(long blockNumber) {
-        this.startBlock = blockNumber;
-        this.endBlock = blockNumber;
-        this.cursor = this.startBlock;
+    @JsonCreator
+    public EthereumBlockRange(
+            @JsonProperty("startBlock") long startBlock,
+            @JsonProperty("endBlock") long endBlock
+    ) {
+        this.startBlock = startBlock;
+        this.endBlock = endBlock;
     }
 
+    @JsonProperty
     public long getStartBlock() {
         return startBlock;
     }
 
+    @JsonProperty
     public long getEndBlock() {
         return endBlock;
-    }
-
-    @Override
-    public boolean hasNext() {
-        return cursor <= endBlock;
-    }
-
-    @Override
-    public Long next() {
-        return cursor++;
     }
 }
--- src/main/java/im/xiaoyao/presto/ethereum/EthereumMetadata.java
+++ src/main/java/im/xiaoyao/presto/ethereum/EthereumMetadata.java
@@ -18,15 +18,19 @@ import com.facebook.presto.spi.predicate.Range;
 import com.facebook.presto.spi.predicate.Ranges;
 import com.facebook.presto.spi.type.ArrayType;
 import com.facebook.presto.spi.type.BigintType;
+import com.facebook.presto.spi.type.CharType;
 import com.facebook.presto.spi.type.DoubleType;
 import com.facebook.presto.spi.type.IntegerType;
 import com.facebook.presto.spi.type.VarcharType;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.airlift.log.Logger;
+import io.airlift.slice.Slice;
 import org.web3j.protocol.Web3j;
 
 import javax.inject.Inject;
+
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -182,15 +186,24 @@ public class EthereumMetadata implements ConnectorMetadata {
                     for (Range r : orderedRanges) {
                         Marker low = r.getLow();
                         Marker high = r.getHigh();
-                        builder.add(new EthereumBlockRange(low, high));
+                        builder.add(EthereumBlockRange.fromMarkers(low, high));
                     }
                 } else if (entry.getKey() instanceof EthereumColumnHandle
                         && (((EthereumColumnHandle) entry.getKey()).getName().equals("block_hash")
                         || ((EthereumColumnHandle) entry.getKey()).getName().equals("tx_blockHash"))) {
-                    web3j.ethGetBlockByHash(entry.getValue().getValues(), true);
-                } else if (entry.getKey() instanceof EthereumColumnHandle
-                        && ((EthereumColumnHandle) entry.getKey()).getName().equals("block_parentHash")) {
-
+                    Ranges ranges = entry.getValue().getValues().getRanges();
+                    List<Range> orderedRanges = ranges.getOrderedRanges();
+                    orderedRanges.stream().filter(Range::isSingleValue).forEach(r -> {
+                        String blockHash = ((Slice) r.getSingleValue()).toStringUtf8();
+                        try {
+                            long blockNumber = web3j.ethGetBlockByHash(blockHash, true).send().getBlock().getNumber().longValue();
+                            builder.add(new EthereumBlockRange(blockNumber, blockNumber));
+                        }
+                        catch (IOException e) {
+                            throw new IllegalStateException("Unable to getting block by hash " + blockHash);
+                        }
+                    });
+                    log.info(entry.getValue().getValues().toString(null));
                 }
             }
         }
